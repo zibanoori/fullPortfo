40fefaeaee931d88baf5ba8b9181ec8aaed75878
zibanoori <noorizibaa@gmail.com>
2026-09-01 21:34:42 +0330

    Set up template directory in Django settings

--- backend/settings.py
+++ backend/settings.py
@@ -45,7 +45,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "template"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
