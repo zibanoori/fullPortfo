ec1aa9a87340af2ed3075303acb10673038db391
zibanoori <noorizibaa@gmail.com>
2026-09-01 21:39:45 +0330

    Create index.html template and update view to render it

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
-    return HttpResponse("<h1>Salammmm</h1>") 
+    return render(request,"index.html") 
