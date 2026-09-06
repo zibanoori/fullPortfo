02120242a0daf6f4506796b55692620ce93b1034
zibanoori <noorizibaa@gmail.com>
2026-09-06 09:16:18 +0330

    Add STATICFILES_DIRS

--- backend/settings.py
+++ backend/settings.py
@@ -107,6 +107,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static", 
+]
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
