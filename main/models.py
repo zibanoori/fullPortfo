b57ce325adecd3e7ba41e908f95ba77c74e90057
zibanoori <noorizibaa@gmail.com>
2026-09-08 20:36:07 +0330

    Use URLField with max_length for social links

--- main/models.py
+++ main/models.py
@@ -3,3 +3,5 @@ from django.db import models
 class main(models.Model):
     logo = models.CharField(max_length=50, blank=True)
     title = models.CharField(max_length=100, blank=True)
+    github_url = models.URLField(max_length=250, blank=True)
+    linkedin_url = models.URLField(max_length=250, blank=True)
