0f94b0492f26d5a0ae2830557700a529b1ca5cec
zibanoori <noorizibaa@gmail.com>
2026-09-08 21:32:38 +0330

    Add help_text to logo field in Main model

--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ class Main(models.Model):
     class Meta:
         verbose_name_plural = "Main Details"
     
-    logo = models.CharField(max_length=50, blank=True)
+    logo = models.CharField(max_length=50, blank=True, help_text="This is a text. Not an image.")
     github_url = models.URLField(max_length=250, blank=True)
     linkedin_url = models.URLField(max_length=250, blank=True)
     copyright = models.CharField(max_length=200, blank=True)
