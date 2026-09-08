a1a46d2bf0279fbb6661399f3278a3adc3055281
zibanoori <noorizibaa@gmail.com>
2026-09-08 20:14:28 +0330

    Addtitle fields to Main model

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
 
 class main(models.Model):
-    logo = models.CharField(max_length=50)
+    logo = models.CharField(max_length=50, blank=True)
+    title = models.CharField(max_length=100, blank=True)
