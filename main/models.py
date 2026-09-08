c03d17fddf8e89060a5b852cc4ea9dec4bada0db
zibanoori <noorizibaa@gmail.com>
2026-09-08 20:43:49 +0330

    Add copyright field to Main model

--- main/models.py
+++ main/models.py
@@ -5,3 +5,4 @@ class main(models.Model):
     title = models.CharField(max_length=100, blank=True)
     github_url = models.URLField(max_length=250, blank=True)
     linkedin_url = models.URLField(max_length=250, blank=True)
+    copyright = models.CharField(max_length=200, blank=True)
