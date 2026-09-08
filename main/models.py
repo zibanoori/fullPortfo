c80c26a800e2479cf247e2802ba642a8d4ff51b6
zibanoori <noorizibaa@gmail.com>
2026-09-08 21:02:40 +0330

    Remove title field from Main model

--- main/models.py
+++ main/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class main(models.Model):
     logo = models.CharField(max_length=50, blank=True)
-    title = models.CharField(max_length=100, blank=True)
     github_url = models.URLField(max_length=250, blank=True)
     linkedin_url = models.URLField(max_length=250, blank=True)
     copyright = models.CharField(max_length=200, blank=True)
