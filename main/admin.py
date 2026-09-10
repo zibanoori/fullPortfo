253bc3f2f889f15ed98f48e7bf8c359d54dc5bb2
zibanoori <noorizibaa@gmail.com>
2026-09-10 22:13:43 +0330

    Register Project model in admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import Main
+from .models import Main, Project
 
 @admin.register(Main)
 class MainAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return not Main.objects.exists()
+    
+    
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'tags']
+    search_fields = ['title', 'description']    
