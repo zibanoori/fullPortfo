f3efec9f4f7b0a623e0491840865cc5fe8c429cd
zibanoori <noorizibaa@gmail.com>
2026-09-12 00:45:15 +0330

    Add dynamic tag splitting in views, update project template structure, and fix URL fie

--- main/views.py
+++ main/views.py
@@ -5,10 +5,16 @@ from .models import Main, Project
 def index(request):
     main_details = Main.objects.first()
     projects = Project.objects.all()
-    
+   
+    for proj in projects:
+        if proj.tags:
+            proj.tag_list = [tag.strip() for tag in proj.tags.split(',')]
+        else:
+            proj.tag_list = []
+
     context = {
-        "details" : main_details,
-        "projects" : projects
+        "details": main_details,
+        "projects": projects
     }
     
-    return render(request,"index.html", context=context) 
+    return render(request, "index.html", context=context)
