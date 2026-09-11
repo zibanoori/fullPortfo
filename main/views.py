29c86e84d32996a51663e966499d33a1eac82108
zibanoori <noorizibaa@gmail.com>
2026-09-11 20:54:57 +0330

    Add Main and Project models, migrations, and index view logic

--- main/views.py
+++ main/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from .models import Main
+from .models import Main, Project
 
 
 def index(request):
     main_details = Main.objects.first()
+    projects = Project.objects.all()
     
     context = {
-        "details" : main_details
+        "details" : main_details,
+        "projects" : projects
     }
     
     return render(request,"index.html", context=context) 
