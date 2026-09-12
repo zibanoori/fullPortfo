1ae8a97c9d1ee36f860430e24dc426539f96df29
zibanoori <noorizibaa@gmail.com>
2026-09-12 21:22:21 +0330

    Organize Main model fields in admin panel

--- main/admin.py
+++ main/admin.py
@@ -6,7 +6,13 @@ class MainAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return not Main.objects.exists()
     
-    list_display = ['title', 'email']
+    fields = [
+        'title', 'logo', 'favicon', 'description',
+        'github_url', 'linkedin_url', 'email', 'address',
+        'about_label', 'about_title', 'about_text', 'journey_title',
+        'skills_title', 'skills_description',
+        'copyright', 'made_with', 'footer_description'
+    ]
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
