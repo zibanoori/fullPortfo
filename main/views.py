ff3d6d143bf0d5c1359c46a60e29166fd0087adf
zibanoori <noorizibaa@gmail.com>
2026-09-12 09:10:53 +0330

    Debug css codes

--- main/views.py
+++ main/views.py
@@ -5,7 +5,7 @@ from .models import Main, Project
 def index(request):
     main_details = Main.objects.first()
     projects = Project.objects.all()
-   
+    
     for proj in projects:
         if proj.tags:
             proj.tag_list = [tag.strip() for tag in proj.tags.split(',')]
